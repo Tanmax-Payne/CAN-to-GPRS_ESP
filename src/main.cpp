7e967754d8e1cea560e639553c5ed76dd97c5f39
Tanmay Mathur <tanmaxigi@gmail.com>
2022-08-15 04:11:37 +0530

    Initial Commit
    Wothout the Methods()

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+void setup() {
+Detect_All_hardware();
+
+Setup_SIM800();
+timeout;
+
+ping_MQTT_Home_Server();
+Get_Key();
+eeprom.write(Port, Keys, Other U_AUTH Tokens);
+
+
+}
+
+
+
+
+void loop() {
+if(HW Dependencies Checkout)
+  {
+    //ASK CAN for data ( store in Strings,)
+    //Filter Out Valid Packets and  
+
+  }
+
+
+
+}
